52c8bb10e8833047fb7d20b5c663b61ec9419d13
jarvisautofiller <jarvis.autoFiller@gmail.com>
2025-07-23 20:03:19 +0530

    Updated swagger specs

--- src/main/java/com/example/jarvis/controller/UserDataController.java
+++ src/main/java/com/example/jarvis/controller/UserDataController.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @RestController
 @RequestMapping("/")
@@ -18,6 +23,19 @@ public class UserDataController {
         this.userService = userService;
     }
 
+
+    @Operation(summary = "Get user details by phone number")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User found",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Object.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid request",
+                    content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content(mediaType = "application/json"))
+    })
     @PostMapping("/lbgUserDetails")
     public ResponseEntity<?> getUserDataByPhoneNumber(@RequestBody IdRequest request) {
         try {
