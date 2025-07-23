5d0a1ffc071d4d20f51e716cd2ce81e1c16d3dbe
jarvisautofiller <jarvis.autoFiller@gmail.com>
2025-07-23 17:13:30 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,7 +7,7 @@ INSERT INTO LBG_USER_DATA (
     INCOME
 )
 VALUES
-('9876543210', 'Software Engineer','1234567','SBI1234','50,00,000'),
+('9866644487', 'Software Engineer','1234567','SBI1234','50,00,000'),
 ('9123456780', 'Data Scientist','2345678','HDFC1234','60,00,000'),
 ('8765432109', 'Product Manager','3456789','ICICI1234','70,00,000'),
 ('7654321098', 'Business Analyst','4567890','AXIS1234','80,00,000'),
